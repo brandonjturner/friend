9c4d762f672fe0cfbf9a6342abf821ff17ae33ee
Brandon Turner <brandon88@gmail.com>
2018-09-01 17:18:07 -0700

    2nd push

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendMatch = [
+    {
+      name: "Cyrus",
+      photo: "https://i.imgur.com/a8iEi1O.jpg",
+      scores: ["2", "2", "2", "2", "2"]
+    }, {
+      name: "Marlo",
+      photo: "https://i.imgur.com/i1V7qQs.jpg",
+      scores: ["3", "3", "3", "3", "3"]
+    }, {
+      name: " Bruce",
+      photo: "https://i.imgur.com/wUjlaNu.jpg",
+      scores: ["1", "1", "1", "1", "1"]
+    }, {
+      name: "Matt",
+      photo: "https://i.imgur.com/GPwTXv3.jpg",
+      scores: ["3", "3", "3", "3", "1"]
+    },
+  ];
+  module.exports = friendMatch;
